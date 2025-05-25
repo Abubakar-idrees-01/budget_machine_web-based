54c0c5ae0bd7ba33c24a6702b5edaf7e36c0d15f
Abubakar2-1 <abubakaridrees804@gmail.com>
2025-05-25 07:55:10 +0500

    finalized

--- budget_machine/users/tests.py
+++ budget_machine/users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserAuthTest(TestCase):
+    def test_user_registration(self):
+        response = self.client.post('/sign_up/', {
+            'username': 'testuser',
+            'password1': 'StrongPass123',
+            'password2': 'StrongPass123'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
